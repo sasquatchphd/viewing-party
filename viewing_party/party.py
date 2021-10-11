6919b100ce1a4d9b8d20bad1936db8bcaa2111d9
Shawn O'Hara <shawnohara@Shawns-MacBook-Pro.local>
2021-10-11 14:17:06 -0700

    can't get the test to run despite the code looking right

---
+++ viewing_party/party.py
@@ -0,0 +1,26 @@
+def create_movie(title, genre, rating):
+    if title and genre and rating:
+        movie = {
+            'title':title,
+            'genre':genre,
+            'rating':rating
+        }
+        return movie
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+
+    user_data = {
+        "watched":[]
+    }
+
+    movie = {
+        'title': 'Title A',
+        'genre': 'Horror',
+        'rating': 3.5
+    }
+
+    user_data["watched"].append(movie)
+
+    return user_data
