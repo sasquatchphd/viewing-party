3732ad1c821f7e06230a917a6031c45686baaba5
sasquatchphd <shawn.p.ohara@gmail.com>
2021-10-13 17:13:45 -0700

    Wave 2 Complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def create_movie(title, genre, rating):
     if title and genre and rating:
         movie = {
@@ -10,15 +12,11 @@ def create_movie(title, genre, rating):
         return None
 
 def add_to_watched(user_data, movie):
-
     user_data["watched"].append(movie)
-
     return user_data
     
 def add_to_watchlist(user_data, movie):
-
     user_data["watchlist"].append(movie)
-
     return user_data
 
 def watch_movie(user_data, title):
@@ -29,6 +27,17 @@ def watch_movie(user_data, title):
     return user_data
 
 
+def get_watched_avg_rating(user_data):
+    ratings = []
+    for movie in user_data["watched"]:
+        ratings.append(movie["rating"])
+    average = sum(ratings) / (len(ratings)) if ratings else 0
+    return(average)
 
-
-
+def get_most_watched_genre(user_data):
+    genres = Counter(g['genre'] for g in user_data['watched'])
+    pop_genre = genres.most_common()
+    return(pop_genre[0][0]) if pop_genre else None
+    
+def get_unique_watched(user_data):
+    
